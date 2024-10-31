ba349cc4f232c3bfaa69cfe41eda9a6c00034dcb
Dattayadav <87682187+Dattayadav@users.noreply.github.com>
2024-10-31 22:14:48 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
+
 typedef struct {
-    int warningEnabled;  // 1 for enabled, 0 for disabled
+    int warningEnabled;         // 1 for enabled, 0 for disabled
     float min;
     float max;
-    float warningTolerance;  // Tolerance value for warnings
+    float warningTolerance;     // Tolerance value for warnings
 } ParameterConfig;
+
 void checkAndPrintWarning(float value, ParameterConfig config, const char* warningMessage) {
-    if (config.warningEnabled) {
-        // Check for warning conditions
-        if (value < config.min + config.warningTolerance) {
-            printf("Warning: Approaching discharge\n");
-        } else if (value > config.max - config.warningTolerance) {
-            printf("Warning: Approaching charge-peak\n");
-        }
+    if (!config.warningEnabled) return;  // Skip if warnings are disabled
+
+    // Unified warning conditions
+    if (value < config.min + config.warningTolerance) {
+        printf("Warning: Approaching discharge\n");
+    } 
+    if (value > config.max - config.warningTolerance) {
+        printf("Warning: Approaching charge-peak\n");
     }
 }
-int isParameterOutOfRange(float value, float min, float max, const char* message) {
-    if (value < min || value > max) {
+
+int isParameterOutOfRange(float value, ParameterConfig config, const char* message) {
+    if (value < config.min || value > config.max) {
         printf("%s\n", message);
         return 1;
     }
     return 0;
 }
+
 int batteryIsOk(float temperature, float soc, float chargeRate, ParameterConfig tempConfig, ParameterConfig socConfig, ParameterConfig chargeRateConfig) {
-    checkAndPrintWarning(temperature, tempConfig, "Temperature out of range!");
-    checkAndPrintWarning(soc, socConfig, "State of Charge out of range!");
-    checkAndPrintWarning(chargeRate, chargeRateConfig, "Charge Rate out of range!");
-    int isTempOutOfRange = isParameterOutOfRange(temperature, tempConfig.min, tempConfig.max, "Temperature out of range!");
-    int isSocOutOfRange = isParameterOutOfRange(soc, socConfig.min, socConfig.max, "State of Charge out of range!");
-    int isChargeRateOutOfRange = isParameterOutOfRange(chargeRate, chargeRateConfig.min, chargeRateConfig.max, "Charge Rate out of range!");
-    return !(isTempOutOfRange || isSocOutOfRange || isChargeRateOutOfRange);
+    float parameters[] = {temperature, soc, chargeRate};
+    ParameterConfig configs[] = {tempConfig, socConfig, chargeRateConfig};
+    const char* warnings[] = {"Temperature out of range!", "State of Charge out of range!", "Charge Rate out of range!"};
+    
+    for (int i = 0; i < 3; i++) {
+        checkAndPrintWarning(parameters[i], configs[i], warnings[i]);
+        if (isParameterOutOfRange(parameters[i], configs[i].min, configs[i].max, warnings[i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
 int main() 
 {
